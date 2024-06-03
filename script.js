fd00f93f81d3e2c4c5b7bc7e1d25a1d7675239a4
jesaiasf <132579764+jesaiasf@users.noreply.github.com>
2024-06-03 08:23:19 -0300

    calculando idade

---
+++ script.js
@@ -0,0 +1,31 @@
+const btnEl = document.getElementById("btn");
+const birthdayEl = document.getElementById("birthday");
+const resultEl = document.getElementById("result");
+
+function calcular() {
+  const birthdayValue = birthdayEl.value;
+  if (birthdayValue === "") {
+    alert("Por favor, insira sua data de nascimento");
+  } else {
+    const age = getAge(birthdayValue);
+    resultEl.innerText = `Sua idade é ${age} ${age > 1 ? "anos" : "ano"}`;
+  }
+}
+
+function getAge(birthdayValue) {
+  const currentDate = new Date();
+  const birthdayDate = new Date(birthdayValue);
+  let age = currentDate.getFullYear() - birthdayDate.getFullYear();
+  const month = currentDate.getMonth() - birthdayDate.getMonth();
+
+  if (
+    month < 0 ||
+    (month === 0 && currentDate.getDate() < birthdayDate.getDate())
+  ) {
+    age--;
+  }
+
+  return age;
+}
+
+btnEl.addEventListener("click", calcular);
